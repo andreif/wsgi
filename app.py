f601a9ef87f694bc017f6991952aefa0bda151ee
Andrei Fokau <andrei.fokau@neutron.kth.se>
2021-08-12 21:21:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-import json
 import os
 import sys
 from wsgiref import simple_server
 
 
-def filter_env(env):
-    return {
-        key: value
-        for key, value in sorted(env.items())
-        if key in ["REMOTE_ADDR"] or key.startswith(("HTTP_", "REQUEST_", "SERVER_PROTOCOL"))
-        if key not in os.environ
-    }
-
-
 def app(environ, start_response):
     start_response("200 OK", [
-        ("Content-type", "application/json; charset=utf-8"),
+        ("Content-type", "text/plain; charset=utf-8"),
         ("Version", sys.version.split()[0]),
     ])
-    return [json.dumps(filter_env(environ), indent=2).encode()]
+    return ["Hello"]
 
 
 simple_server.ServerHandler.server_software = ""
